47a6d9bd1b21c930fa831f82bb936e53d60d3203
dhlabanu <hlabanud1@gmail.com>
2025-08-22 16:20:32 +0200

    Initial commit

---
+++ src/test/java/com/javatechie/spring/api/GitTestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatechie.spring.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTestAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
